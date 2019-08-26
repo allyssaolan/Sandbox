5738731fc5084b6e6a74a3eda064424d9d269bef
allyssa <allyssa.olan@my.jcu.edu.au>
2019-08-26 11:58:56 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,8 @@
 """Allyssa Olan"""
+
+min_length = 4
+
+password = input("Enter valid password: ")
+while len(password) < min_length:
+    password = input("Invalid password. Please enter a valid password: " )
+print((len(password) * '*'))
